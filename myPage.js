796fa438551d2fc2e0ee25735775eec2c8fc672d
jcyoder <kajcyoder@comcast.net>
2014-04-30 21:45:50 -0600

    added js to change nav background on mouseenter and mouseleave

--- myPage.js
+++ myPage.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
-    $('.navButtons').mouseenter(funcion() {
-         $(this).css('background-color: #4b4b4b');                       
+    $('.navButtons').mouseenter(function () {
+        $(this).css('background', '#4b4b4b');
     });
+    
+    $('.navButtons').mouseleave(function () {
+        $(this).css('background', 'none');
+    });
+ //   $('.navtext').mouseenter(function () {
+//        $('this').css('color', 'white');
+  //  });
+    
     
 });

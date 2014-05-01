a9eb3b3687ac1bca432faf8c3686ff375f6e7a10
jcyoder <kajcyoder@comcast.net>
2014-04-30 21:52:48 -0600

    added code to change text color

--- myPage.js
+++ myPage.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
     $('.navButtons').mouseenter(function () {
         $(this).css('background', '#4b4b4b');
+        $(this).children('.navtext').css('color', 'white');
     });
     
     $('.navButtons').mouseleave(function () {
         $(this).css('background', 'none');
+        $(this).children('.navtext').css('color', 'black');
     });
  //   $('.navtext').mouseenter(function () {
 //        $('this').css('color', 'white');

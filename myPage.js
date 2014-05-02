927adeaccfce2954934dff62c316f24bd8820fb4
jcyoder <kajcyoder@comcast.net>
2014-05-01 18:01:17 -0600

    added submenus for attractions menu, repositions title and nav

--- myPage.js
+++ myPage.js
@@ -2,15 +2,15 @@ $(document).ready(function () {
     $('.navButtons').mouseenter(function () {
         $(this).css('background', '#4b4b4b');
         $(this).children('.navtext').css('color', '#66ff66');
+        $(this).children('.submenu').slideToggle('fast');
     });
     
     $('.navButtons').mouseleave(function () {
         $(this).css('background', 'none');
         $(this).children('.navtext').css('color', 'black');
+        $(this).children('.submenu').slideToggle('fast');
     });
- //   $('.navtext').mouseenter(function () {
-//        $('this').css('color', 'white');
-  //  });
+
     
     
 });

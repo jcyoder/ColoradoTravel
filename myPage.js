39ecea4c4d03387d16edc9a2899c1732ca46b85e
jcyoder <kajcyoder@comcast.net>
2014-05-03 11:22:15 -0600

    added js to change colors when submenus are hovered over and logo changes

--- myPage.js
+++ myPage.js
@@ -3,14 +3,25 @@ $(document).ready(function () {
         $(this).css('background', '#4b4b4b');
         $(this).children('.navtext').css('color', '#66ff66');
         $(this).children('.submenu').slideToggle('fast');
+       
     });
     
     $('.navButtons').mouseleave(function () {
         $(this).css('background', 'none');
         $(this).children('.navtext').css('color', 'black');
         $(this).children('.submenu').slideToggle('fast');
+      // $(this).children('.submenu .submenuElem').css('background', '#4b4b4b');
     });
 
+    $('.submenuElem').mouseenter(function () {
+        $(this).css('background', 'green');
+        $(this).css('color', 'white');
+        
+    });
     
+    $('.submenuElem').mouseleave(function () {
+        $(this).css('background', '#4b4b4b');
+        $(this).css('color', '#66ff66');
+    });
     
 });
